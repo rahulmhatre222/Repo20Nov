2d057bb4142ca4a86e66935e4ffa3536e6321eb2
rahul <rahul@DESKTOP-VU6JIBV>
2022-11-20 13:01:45 +0530

    First Commit

---
+++ githubpro/src/test/java/githubpro/TestClass1.java
@@ -0,0 +1,16 @@
+package githubpro;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Tc01");
+		System.out.println("Tc01");
+		System.out.println("Tc01");
+		System.out.println("Tc01");
+		System.out.println("Tc01");
+		
+		
+	}
+
+}
